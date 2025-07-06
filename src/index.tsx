f8c32774a945d4dd0d8d001c1ef23766f920fe50
Stanislav Snizhkovyi <stas.snezhkovoy@gmail.com>
2025-07-07 01:34:12 +0300

    fix routes name

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/Routes.tsx';
+import { router } from './Routes/Routes';
 import './index.css';
 import './styles/global.scss';
 
