3941f84aa7fd9933f1972d6fb1cbc6f0a374bd8f
Andrii Zakharov <zaharov.andrey.93@gmail.com>
2025-07-06 16:02:41 +0300

    update header

--- src/components/LayoutParts/Header/Header.tsx
+++ src/components/LayoutParts/Header/Header.tsx
@@ -5,23 +5,25 @@ import { Link } from 'react-router-dom';
 export const Header: FC = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>Header</div>
-      <nav>
-        <ul className={styles.navList}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/phones">Phones</Link>
-          </li>
-          <li>
-            <Link to="/tablets">Tablets</Link>
-          </li>
-          <li>
-            <Link to="/accessories">Accessories</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.container}>
+        Header
+        <nav>
+          <ul className={styles.navList}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/phones">Phones</Link>
+            </li>
+            <li>
+              <Link to="/tablets">Tablets</Link>
+            </li>
+            <li>
+              <Link to="/accessories">Accessories</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
