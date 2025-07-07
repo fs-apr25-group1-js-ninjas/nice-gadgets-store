51726244f1d9d5cd1887a82ab421829551b50bc4
Andrii Zakharov <zaharov.andrey.93@gmail.com>
2025-07-08 00:30:24 +0300

    chore(HOME.5) commited and update useEffect in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
-import { useEffect, type FC } from 'react';
+import { type FC } from 'react';
 
 import { Header } from './components/LayoutParts/Header';
 import { Main } from './components/Main';
 import { Footer } from './components/LayoutParts/Footer';
-import { client } from './utils/fetchProducts';
-import type { ProductsApiResponse } from './types/product';
-
-const getProducts = () => {
-  return client.get<ProductsApiResponse>('./api/products.json');
-};
 
 export const App: FC = () => {
   // const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const productsFromServer = await getProducts();
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     try {
+  //       const productsFromServer = await getProducts();
 
-        // setProducts(productsFromServer);
-        console.log(productsFromServer); // delete this console.log
-      } catch (error) {
-        console.log(error); // change this console.log to 'throw New error'
-      }
-    };
+  //       // setProducts(productsFromServer);
+  //       console.log(productsFromServer); // delete this console.log
+  //     } catch (error) {
+  //       console.log(error); // change this console.log to 'throw New error'
+  //     }
+  //   };
 
-    fetchTodos();
-  }, []);
+  //   fetchProducts();
+  // }, []);
 
   return (
     <>
