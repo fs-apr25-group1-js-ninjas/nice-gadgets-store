83936f9f5042bf7988915c2d10edd7bb78fd78ea
Stanislav Snizhkovyi <stas.snezhkovoy@gmail.com>
2025-07-07 15:06:21 +0300

    feat(DEV-0001): add fetch products, change location folder Api, add types of Products

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import type { FC } from 'react';
+import { useEffect, type FC } from 'react';
 
 import { Header } from './components/LayoutParts/Header';
 import { Main } from './components/Main';
 import { Footer } from './components/LayoutParts/Footer';
+import { client } from './utils/fetchProducts';
+import type { ProductsApiResponse } from './types/product';
 
-export const App: FC = () => (
-  <>
-    <Header />
-    <Main />
-    <Footer />
-  </>
-);
+const getProducts = () => {
+  return client.get<ProductsApiResponse>('./api/products.json');
+};
+
+export const App: FC = () => {
+  // const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const productsFromServer = await getProducts();
+
+        // setProducts(productsFromServer);
+        console.log(productsFromServer); // delete this console.log
+      } catch (error) {
+        console.log(error); // change this console.log to 'throw New error'
+      }
+    };
+
+    fetchTodos();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+};
