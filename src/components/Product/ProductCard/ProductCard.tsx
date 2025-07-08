5af07355e0b0419f6013c467f34075b68865ed60
Jaroslava <lazyrka@gmail.com>
2025-07-08 09:05:09 +0300

    feat(HOME.3): add initial markup product card

--- src/components/Product/ProductCard/ProductCard.tsx
+++ src/components/Product/ProductCard/ProductCard.tsx
@@ -1,9 +1,66 @@
-import type { FC } from 'react';
+// import { Card } from '../../Common/Card';
 
-import { Card } from '../../Common/Card';
+import type { FC } from 'react';
+import iphone7Black00 from '../../../assets/images/phones/apple-iphone-7/black/00.webp';
 
 import styles from './ProductCard.module.scss';
 
-export const ProductCard: FC = () => {
-  return <Card className={styles.product}>Product Card</Card>;
+// import type { Product } from '../../../types/product';
+import { Link } from 'react-router-dom';
+import type { Product } from '../../../types/product';
+
+interface Props {
+  product?: Product;
+}
+
+export const ProductCard: FC<Props> = () => {
+  const product = {
+    category: 'phones',
+    itemId: 'apple-iphone-7-32gb-black',
+    name: 'Apple iPhone 7 32GB Black',
+    price: 375,
+    screen: "4.7' IPS",
+    capacity: '32GB',
+    ram: '2GB',
+    image: iphone7Black00,
+  };
+  return (
+    <div className={styles.card}>
+      <Link
+        to={`/${product.category}/${product.itemId}`}
+        className={styles.cardLink}
+      >
+        <img
+          src={product.image}
+          alt={product.name}
+          className={styles.image}
+        />
+
+        <p className={styles.name}>{product.name}</p>
+
+        <div className={styles.prices}>
+          <p className={styles.price}>${product.price}</p>
+        </div>
+
+        <div className={styles.divider} />
+
+        <div className={styles.specs}>
+          <div className={styles.row}>
+            <span className={styles.key}>Screen</span>
+            <span className={styles.value}>{product.screen}</span>
+          </div>
+          <div className={styles.row}>
+            <span className={styles.key}>Capacity</span>
+            <span className={styles.value}>{product.capacity}</span>
+          </div>
+          <div className={styles.row}>
+            <span className={styles.key}>RAM</span>
+            <span className={styles.value}>{product.ram}</span>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+
+  // return <Card className={styles.ProductCard}>Product Card</Card>;
 };
--- src/components/Sections/BrandNewModelsSection/BrandNewModelsSection.tsx
+++ src/components/Sections/BrandNewModelsSection/BrandNewModelsSection.tsx
@@ -1,11 +1,26 @@
 import type { FC } from 'react';
 import styles from './BrandNewModelsSection.module.scss';
 import { ProductsSlider } from '../../Sliders/ProductsSlider';
+import { ProductCard } from '../../Product/ProductCard';
+// import { ProductCard } from '../../Product/ProductCard';
+// import type { Product } from '../../../types/product';
+
+// type Props = {
+//   products: Product[];
+// };
 
 export const BrandNewModelsSection: FC = () => {
   return (
     <section className={styles.brandNewModelsSection}>
       <h2>Brand new models</h2>
+      <ProductCard />
+
+      {/* {products.map(product => (
+  <ProductCard
+    key={product.id}
+    product={product}
+  />
+))} */}
       <ProductsSlider />
     </section>
   );
