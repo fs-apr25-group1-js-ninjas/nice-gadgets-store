2d40de02f9db2ffdbad7eb86662e677d2ddc4b88
Jaroslava <lazyrka@gmail.com>
2025-07-09 15:38:21 +0300

    fix(HOME.3) add props interface, product card, product fetch,  font fix

--- src/components/Sections/ProductsListSection/ProductsListSection.tsx
+++ src/components/Sections/ProductsListSection/ProductsListSection.tsx
@@ -1,11 +1,16 @@
 import type { FC } from 'react';
 import styles from './ProductsListSection.module.scss';
 import { ProductCard } from '../../Product/ProductCard';
+import type { Product } from '../../../types/product';
 
-export const ProductsListSection: FC = () => {
+interface Props {
+  product: Product;
+}
+
+export const ProductsListSection: FC<Props> = ({ product }) => {
   return (
     <div className={styles.productsList}>
-      <ProductCard />
+      <ProductCard product={product} />
     </div>
   );
 };
