b2bd55ea9367bfd4d766dfa8d29e03a50b46c57a
Stanislav Snizhkovyi <stas.snezhkovoy@gmail.com>
2025-07-09 19:01:48 +0300

    fix(HOME.2): fix HeroSlider pagination

--- src/components/Sliders/HeroSlider/HeroSlider.tsx
+++ src/components/Sliders/HeroSlider/HeroSlider.tsx
@@ -1,19 +1,17 @@
 import type { FC } from 'react';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 //@ts-expect-error: Swiper CSS has no TS types
 import 'swiper/scss';
-
 import ArrowLeft from '/icons/arrow_left_active.svg';
 import ArrowRight from '/icons/arrow_right_active.svg';
 import phonesBanner from '/img/banner-phones.png';
 import tabletsBanner from '/img/banner-tablets.png';
 import accessoriesBanner from '/img/banner-accessories.png';
-
 import styles from './HeroSlider.module.scss';
-import { Link } from 'react-router-dom';
-import { useMediaQuery } from 'react-responsive';
 
 type ProductCategoryType = 'phones' | 'tablets' | 'accessories';
 
@@ -43,8 +41,14 @@ const categoryBanners: categoryItem[] = [
 
 export const HeroSlider: FC = () => {
   const paginationRef = useRef<HTMLDivElement>(null);
-
+  const [swiperReady, setSwiperReady] = useState(false);
   const isLargeScreen = useMediaQuery({ minWidth: 640 });
+
+  useEffect(() => {
+    if (paginationRef.current) {
+      setSwiperReady(true);
+    }
+  }, []);
 
   return (
     <div className={styles.heroSlider}>
@@ -58,45 +62,47 @@ export const HeroSlider: FC = () => {
           </button>
         )}
 
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={0}
-          loop={true}
-          autoplay={{ delay: 5000 }}
-          navigation={{
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          }}
-          pagination={{
-            clickable: true,
-            el: paginationRef.current!,
-          }}
-          modules={[Pagination, Navigation, Autoplay]}
-          onBeforeInit={(swiper) => {
-            // @ts-expect-error — for TypeScript
-            swiper.params.pagination.el = paginationRef.current;
-          }}
-          className={styles.swiper}
-          style={{
-            width: '100%',
-            height: '100%',
-          }}
-        >
-          {categoryBanners.map(({ category, label, image }) => (
-            <SwiperSlide key={label}>
-              <Link
-                to={category}
-                style={{ textDecoration: 'none' }}
-              >
-                <img
-                  src={image}
-                  alt="Slide"
-                  className="swiper-img"
-                />
-              </Link>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        {swiperReady && (
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={0}
+            loop={true}
+            autoplay={{ delay: 5000 }}
+            navigation={{
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            }}
+            pagination={{
+              clickable: true,
+              el: paginationRef.current!,
+            }}
+            modules={[Pagination, Navigation, Autoplay]}
+            onBeforeInit={(swiper) => {
+              // @ts-expect-error — for TypeScript
+              swiper.params.pagination.el = paginationRef.current;
+            }}
+            className={styles.swiper}
+            style={{
+              width: '100%',
+              height: '100%',
+            }}
+          >
+            {categoryBanners.map(({ category, label, image }) => (
+              <SwiperSlide key={label}>
+                <Link
+                  to={category}
+                  style={{ textDecoration: 'none' }}
+                >
+                  <img
+                    src={image}
+                    alt="Slide"
+                    className="swiper-img"
+                  />
+                </Link>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
 
         {isLargeScreen && (
           <button className="swiper-button-next">
