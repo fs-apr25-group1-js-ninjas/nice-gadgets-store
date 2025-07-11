3fc12dc7d84ca5839a6a01b606cbdf922bb4c4f9
Jaroslava <lazyrka@gmail.com>
2025-07-11 10:23:43 +0300

    feat(HOME.4.1)fix to card, fix to slider, logic for new products, hot products

--- src/components/Product/ProductCard/ProductCard.tsx
+++ src/components/Product/ProductCard/ProductCard.tsx
@@ -9,6 +9,7 @@ import { FavoritesButton } from '../FavoritesButton/FavoritesButton';
 
 interface Props {
   product: Product;
+  showFullPrice?: boolean;
 }
 
 export const ProductCard: FC<Props> = ({ product }) => {
@@ -31,46 +32,52 @@ export const ProductCard: FC<Props> = ({ product }) => {
 
   return (
     <div className={styles.card}>
-      <Link
-        to={`/${product.category}/${product.itemId}`}
-        className={styles.cardLink}
-      >
-        <img
-          src={product.image}
-          alt={product.name}
-          className={styles.image}
-        />
+      <div className={styles.cardEffect}>
+        <Link
+          to={`/${product.category}/${product.itemId}`}
+          className={styles.cardLink}
+        >
+          <img
+            src={product.image}
+            alt={product.name}
+            className={styles.image}
+          />
 
-        <div className={styles.name}>
-          <p>{product.name}</p>
-        </div>
-
-        <div className={styles.prices}>
-          <p className={styles.price}>${product.price}</p>
-        </div>
-
-        <div className={styles.divider} />
-
-        <div className={styles.specs}>
-          <div className={styles.row}>
-            <span className={styles.key}>Screen</span>
-            <span className={styles.value}>{product.screen}</span>
+          <div className={styles.name}>
+            <p>{product.name}</p>
           </div>
-          <div className={styles.row}>
-            <span className={styles.key}>Capacity</span>
-            <span className={styles.value}>{product.capacity}</span>
+
+          <div className={styles.prices}>
+            <p className={styles.price}>${product.price}</p>
+
+            {product.fullPrice > product.price && (
+              <p className={styles.fullPrice}>${product.fullPrice}</p>
+            )}
           </div>
-          <div className={styles.row}>
-            <span className={styles.key}>RAM</span>
-            <span className={styles.value}>{product.ram}</span>
+
+          <div className={styles.divider} />
+
+          <div className={styles.specs}>
+            <div className={styles.row}>
+              <span className={styles.key}>Screen</span>
+              <span className={styles.value}>{product.screen}</span>
+            </div>
+            <div className={styles.row}>
+              <span className={styles.key}>Capacity</span>
+              <span className={styles.value}>{product.capacity}</span>
+            </div>
+            <div className={styles.row}>
+              <span className={styles.key}>RAM</span>
+              <span className={styles.value}>{product.ram}</span>
+            </div>
           </div>
+        </Link>
+
+        <div className={styles.actions}>
+          <AddToCartButton onClick={addToCart} />
+
+          <FavoritesButton onClick={addToFavorites} />
         </div>
-      </Link>
-
-      <div className={styles.actions}>
-        <AddToCartButton onClick={addToCart} />
-
-        <FavoritesButton onClick={addToFavorites} />
       </div>
     </div>
   );
