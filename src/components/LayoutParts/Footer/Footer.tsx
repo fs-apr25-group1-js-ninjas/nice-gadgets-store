6d0c2dc7b77d24087e2b6acec4ff3fca40cccc49
Yura Soroka <ykotov18@gmail.com>
2025-07-13 19:11:59 +0300

    fix(FAV2.1): remade grid in favourite, border in footer, back button in cart

--- src/components/LayoutParts/Footer/Footer.tsx
+++ src/components/LayoutParts/Footer/Footer.tsx
@@ -9,55 +9,53 @@ const scrollToTop = () => {
 
 export const Footer: FC = () => (
   <footer className={styles.footer}>
-    <div className="container">
-      <div className={styles.footerContent}>
-        <div className={styles.footerLogo}>
-          <a href="#">
-            <img
-              src={logo}
-              alt="NICE GADGETS LOGO"
-              className={styles.logoImage}
-            />
-          </a>
-        </div>
+    <div className={styles.footerContent}>
+      <div className={styles.footerLogo}>
+        <a href="#">
+          <img
+            src={logo}
+            alt="NICE GADGETS LOGO"
+            className={styles.logoImage}
+          />
+        </a>
+      </div>
 
-        <nav className={styles.footerNavigation}>
-          <a
-            href="#"
-            className={styles.navLink}
-          >
-            Github
-          </a>
-          <a
-            href="#"
-            className={styles.navLink}
-          >
-            Contacts
-          </a>
-          <a
-            href="#"
-            className={styles.navLink}
-          >
-            Rights
-          </a>
-        </nav>
-
-        <div
-          className={styles.backToTopGroup}
-          onClick={scrollToTop}
+      <nav className={styles.footerNavigation}>
+        <a
+          href="#"
+          className={styles.navLink}
         >
-          <span className={styles.backToTopText}>Back to top</span>
-          <button
-            className={styles.backToTopButton}
-            aria-label="Back to top"
-          >
-            <img
-              src={arrowUpIcon}
-              alt="Up arrow"
-              className={styles.arrowIcon}
-            />
-          </button>
-        </div>
+          Github
+        </a>
+        <a
+          href="#"
+          className={styles.navLink}
+        >
+          Contacts
+        </a>
+        <a
+          href="#"
+          className={styles.navLink}
+        >
+          Rights
+        </a>
+      </nav>
+
+      <div
+        className={styles.backToTopGroup}
+        onClick={scrollToTop}
+      >
+        <span className={styles.backToTopText}>Back to top</span>
+        <button
+          className={styles.backToTopButton}
+          aria-label="Back to top"
+        >
+          <img
+            src={arrowUpIcon}
+            alt="Up arrow"
+            className={styles.arrowIcon}
+          />
+        </button>
       </div>
     </div>
   </footer>
