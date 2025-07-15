c0259f04251e19c202202af1e0730d0d827b12f3
Stanislav Snizhkovyi <stas.snezhkovoy@gmail.com>
2025-07-15 15:50:13 +0300

    fix(FIX.HOOK) fix hook useFetchProducts | clean-up styles HeroSlider

--- src/hooks/useFetchProducts.ts
+++ src/hooks/useFetchProducts.ts
@@ -2,21 +2,26 @@ import { useEffect, useState } from 'react';
 import type { Product } from '../types/product';
 import { getProducts } from '../utils/getProducts';
 
+const API = './api/products.json';
+
 export const useFetchProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchAllProducts = async () => {
       try {
-        const data = await getProducts<Product[]>(`./api/products.json`);
+        const data = await getProducts<Product[]>(API);
         setProducts(data);
       } catch (error) {
         console.error('Cannot load products', error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchAllProducts();
   }, []);
 
-  return { products };
+  return { products, isLoading };
 };
