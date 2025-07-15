31a255a0d0edc4103c8e2051131e0485d8073828
Stanislav Snizhkovyi <stas.snezhkovoy@gmail.com>
2025-07-15 20:56:10 +0300

    feat(DB.1): add Firebase DB | add AuthPage | fix Pagination

--- src/utils/fetchProducts.ts
+++ src/utils/fetchProducts.ts
@@ -1,34 +1,34 @@
+import {
+  fetchAllProducts,
+  fetchProductsByCategory,
+  fetchDetailedProduct,
+  fetchFromCollection,
+} from './firebaseUtils';
+
 function wait(delay: number) {
   return new Promise((resolve) => {
     setTimeout(resolve, delay);
   });
 }
 
-function request<T>(
-  url: string,
-  method = 'GET',
-  data: unknown = null,
-): Promise<T> {
-  const options: RequestInit = { method };
-
-  if (data) {
-    options.body = JSON.stringify(data);
-    options.headers = {
-      'Content-Type': 'application/json; charset=UTF-8',
-    };
-  }
-
-  return wait(100)
-    .then(() => fetch(url, options))
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error();
-      }
-
-      return response.json();
-    });
-}
-
 export const client = {
-  get: <T>(url: string) => request<T>(url),
+  getAllProducts: async () => {
+    await wait(100);
+    return fetchAllProducts();
+  },
+
+  getProductsByCategory: async (category: string) => {
+    await wait(100);
+    return fetchProductsByCategory(category);
+  },
+
+  getDetailedProduct: async (category: string, itemId: string) => {
+    await wait(100);
+    return fetchDetailedProduct(category, itemId);
+  },
+
+  getFromCollection: async (collectionName: string) => {
+    await wait(100);
+    return fetchFromCollection(collectionName);
+  },
 };
